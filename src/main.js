ce50dd93a39df3fe607a644ebe3e67ea6903f4f5
Alexandre <alexandre.d.s.gaspar@gmail.com>
2022-10-20 21:08:54 -0300

    Project Finished

--- src/main.js
+++ src/main.js
@@ -82,8 +82,37 @@ document.querySelector("form").addEventListener("submit", (event) => {
 })
 
 const cardHolder = document.querySelector("#card-holder")
-cardHolder.addEventListener("imput", () => {
+cardHolder.addEventListener("input", () => {
   const ccHolder = document.querySelector(".cc-holder .value")
 
-  ccHolder.innerText = cardHolder.value
+  ccHolder.innerText =
+    cardHolder.value.length === 0 ? "FULANO DA SILVA" : cardHolder.value
 })
+
+securityCodeMask.on("accept", () => {
+  updateSecurityCode(securityCodeMask.value)
+})
+
+function updateSecurityCode(code) {
+  const ccSecurity = document.querySelector(".cc-security .value")
+
+  ccSecurity.innerText = code.length === 0 ? "123" : code
+}
+
+cardNumberMasked.on("accept", () => {
+  const cardType = cardNumberMasked.masked.currentMask.cardType
+  setCardType(cardType)
+  updateCardNumber(cardNumberMasked.value)
+})
+function updateCardNumber(number) {
+  const ccNumber = document.querySelector(".cc-number")
+  ccNumber.innerText = number.length === 0 ? "1234 5678 9012 3456" : number
+}
+
+expirationDateMask.on("accept", () => {
+  updadeExpirationDate(expirationDateMask.value)
+})
+function updadeExpirationDate(date) {
+  const ccExpiration = document.querySelector(".cc-extra .value")
+  ccExpiration.innerText = date.length === 0 ? "02/32" : date
+}
